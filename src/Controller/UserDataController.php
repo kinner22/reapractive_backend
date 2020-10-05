7e7471892dbd11e0ffa6c1587a7a10fad8a2bfa5
Kinner <kinner22@gmail.com>
2020-10-05 04:30:37 +0200

    CRUD create UserData

--- src/Controller/UserDataController.php
+++ src/Controller/UserDataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\UserData;
 use App\Repository\UserDataRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -22,11 +23,36 @@ class UserDataController extends AbstractController
     }
 
 
-    public function create(Request $request): JsonResponse
+    public function createUserData(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
         $data['currentTime']  = date("Y m d H:i:s");
-        return new JsonResponse($data);
+
+        try {
+            $em = $this->getDoctrine()->getManager();
+            $userData = new UserData();
+            $userData->setName($data['name']);
+            $userData->setUsername($data['username']);
+            $userData->setSurname($data['surname']);
+
+            $em->persist($userData);
+
+            $em->flush();
+        } catch (\Exception $e) {
+            return new JsonResponse([
+                'status' => '400',
+                'data' => [
+                    'message' => $e->getMessage()
+                ]
+            ], 400);
+        }
+
+        return new JsonResponse([
+            'status' => 'OK',
+            'data' => [
+                'id' => $userData->getId()
+            ]
+        ]);
     }
 
     public function update()
